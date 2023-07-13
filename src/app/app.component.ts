dbfa18ba141e5c0066bf5045a40703b039eb28d9
José María Rinaldi <115929308+JoseRinaldi@users.noreply.github.com>
2023-07-13 12:59:29 -0300

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-todo-list';
+  /* An empty array that is responsible
+       to add a division */
+       public items:string[] = [];
+
+       /* A two-way binding performed which
+          pushes text on division */
+       public newTask:string = '';
+
+
+       /* When input is empty, it will
+          not create a new division */
+       public addToList():void {
+           if (this.newTask === '') {
+           }
+           else {
+               this.items.push(this.newTask);
+               this.newTask = '';
+           }
+       }
+
+    //    public addToListEnter(value:string):void {
+    //     this.newTask = value;
+
+    //     if (this.newTask === '') {
+    //     }
+    //     else {
+    //         this.items.push(this.newTask);
+    //         this.newTask = '';
+    //     }
+    // }
+
+       /* This function takes to input the
+          task, that has to be deleted*/
+       public deleteTask(index:number) :void{
+           this.items.splice(index, 1);
+       }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shader/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
