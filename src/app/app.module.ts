42822fc18f3eeca2f666755ad3d3e608380615dc
José María Rinaldi <115929308+JoseRinaldi@users.noreply.github.com>
2023-07-13 13:40:33 -0300

    to do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './shader/sidebar/sidebar.component';
+import { NavbarComponent } from './shader/navbar/navbar.component';
+import { ShaderModule } from './shader/shader.module';
+import { InputAddComponent } from './todo/components/input-add/input-add.component';
+import { TaskComponent } from './todo/components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    InputAddComponent,
+    TaskComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ShaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
